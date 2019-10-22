7d2ce2c1bf3040302579ec82b733730fcb71ae6d
TaeHwoi Gu <47591363+zeynut@users.noreply.github.com>
2019-10-22 18:02:33 +0900

    signup 비번,term검증,useInput_customhooks

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -5,35 +5,42 @@ import { Form, Checkbox ,Input, Button } from 'antd';
 
 const Signup = () => {
     
-    const [id, setId] = useState('');
-    const [nick, setNick] =  useState('');
-    const [password, setPassword] = useState('');
     const [passwordCheck , setPasswordCheck] = useState('');
     const [term , setTerm ] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [termError, setTermError] = useState(false);
+
+    const useInput = (initValue = null ) => {
+        const [ value , setter ] = useState(initValue);
+        const handler = (e) =>{
+            setter(e.target.value);
+        }
+        return [ value , handler ];
+    }
+      
+    const [ id , onChangeId ] = useInput('');
+    const [ nick , onChangeNick ] = useInput('');
+    const [ password , onChangePassword ] = useInput('');    
 
     const onSubmit = (e) => {
         e.preventDefault();
         console.log({id,nick,password,passwordCheck,term});
+        
+        if(password !== passwordCheck){
+            return setPasswordError(true);
+        }
+        if(!term){
+            return setTermError(true);
+        }
     }
-    
-    const onChangeId = (e) => {
-        setId(e.target.value);
-    }
-
-    const onChangeNick = (e) => {
-        setNick(e.target.value);
-    }
-
-    const onChangePassword = (e) => {
-        setPassword(e.target.value);
-    
-    }
-
+     
     const onChangePasswordChk = (e) => {
+        setPasswordError(e.target.value !== password);
         setPasswordCheck(e.target.value);
     }
 
     const onChangeTerm = (e) => {
+        setTermError(false);
         setTerm(e.target.checked);
     }
 
@@ -65,11 +72,13 @@ const Signup = () => {
                     <label htmlFor="user-password-check">비밀번호체크</label>
                     <br/>
                     <Input name="user-password-check" value={passwordCheck} type="password" required onChange={onChangePasswordChk}></Input>
+                    {passwordError && <div style={{ color: 'red' }}>비번이 일치하지 않습니다.</div>}
                 </div>
                 <div>
                     <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>동의합니다.</Checkbox>
+                    {termError && <div style={{ color: 'red'}}> 약관에 동의해주세요.</div>}
                 </div>
-                <div>
+                <div style={{ marginTop: 10}}>
                     <Button type="danger" htmlType="submit">가입하기</Button>
                 </div>
             </Form>
